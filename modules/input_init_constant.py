89ba25de48999d110e4e43f316c94dfef394cbec
HKNeuro <hdjk201@exeter.ac.uk>
2022-12-06 00:51:21 +0000

    Create input_init_constant.py
    For when you just want constant input everywhere, typically because you're importing existing values for v and s.

---
+++ modules/input_init_constant.py
@@ -0,0 +1,20 @@
+"""
+Sets the input current array be equal everywhere.
+"""
+
+from numba import cuda
+from math import pi, e
+
+from modules import device_gaussian
+from modules import Control
+lookup = Control.lookup
+
+neurons_number = lookup["neurons_number"]
+strength = lookup["input_base_before"]
+
+@cuda.jit()
+def input_init(input_strength_d, coordinates_d):
+    """Initialises input_strength array with a constant value."""
+    n = cuda.grid(1)
+    if n < neurons_number:
+        input_strength_d[n] = strength
