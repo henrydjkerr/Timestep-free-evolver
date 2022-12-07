a3f0b9ed100879d943667b5fa7fe7f38e6e1f1cb
HKNeuro <hdjk201@exeter.ac.uk>
2022-12-07 16:40:16 +0000

    Update testing v equations.py
    Adding in an even more basic level of numerical simulation to verify the equations for v and s.

--- side-tools/testing v equations.py
+++ side-tools/testing v equations.py
@@ -12,8 +12,7 @@ b = 1
 Delta = 1
 sqrt2 = 2**0.5
 c = 3.48
-c_v = 3.686
-c_s = 3.686
+
 
 def part_v(Z, z, t):
     coeff = Z*z * e**((z*beta / (c * sqrt2))**2)
@@ -38,25 +37,50 @@ def s(t):
             (part_s(A, a, t) - part_s(B, b, t))
     return value
 
-points = 50
+def part_connect(Z, z, t):
+    #value = (Z / ((2*pi)**0.5 * z)) * e**(-0.5 * (c*t / z)**2)
+    value = Z * e**(-0.5 * (c*t / z)**2)
+    return value
+
+def connect(t):
+    return part_connect(A, a, t) - part_connect(B, b, t)    
+
+points = 500
 lower_bound = -2
+step = abs(lower_bound)/points
+
 t_values = numpy.linspace(lower_bound, 0, points)
 v_analytic = numpy.zeros(points)
 v_numeric = numpy.zeros(points)
+v_very_numeric = numpy.zeros(points)
+s_temp = 0
 for k in range(points):
+    #v_analytic
     v_analytic[k] = v(t_values[k])
+    #v_numeric
     if k == 0:
         v_numeric[k] = I
     else:
         v_numeric[k] = v_numeric[k - 1] \
-                       + abs(lower_bound)/points \
-                       * (I - v_numeric[k - 1] + s(t_values[k - 1]))
+                       + step * (I - v_numeric[k - 1] + s(t_values[k - 1]))
+    #v_very_numeric
+    if k == 0:
+        v_very_numeric[k] = I
+    else:
+        #s_temp *= e**(-beta * step)
+        #s_temp += step * beta * connect(t_values[k])
+        s_temp += step * beta * (connect(t_values[k]) - s_temp)
+        v_very_numeric[k] = v_very_numeric[k - 1] \
+                            + step * (I - v_numeric[k - 1] + s_temp)
     
 
 plt.figure()
 plt.plot(t_values, v_analytic)
 plt.plot(t_values, v_numeric)
+plt.plot(t_values, v_very_numeric)
 plt.axhline(y = 0)
 plt.axhline(y = 1)
 plt.axvline(x = 0)
+plt.xlabel("t (= x/c)")
+plt.ylabel("v(t)")
 plt.show()
