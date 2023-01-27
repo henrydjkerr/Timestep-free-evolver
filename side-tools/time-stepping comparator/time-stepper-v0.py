e1f9e2e022fc9a93bfc6ad293b74423a8b681e2c
HKNeuro <hdjk201@exeter.ac.uk>
2023-01-27 00:37:17 +0000

    Update time-stepper-v0.py
    Corrections, checks, and writing alt code independent of library.

--- side-tools/time-stepping comparator/time-stepper-v0.py
+++ side-tools/time-stepping comparator/time-stepper-v0.py
@@ -28,6 +28,8 @@ leniency_threshold = lookup["leniency_threshold"]
 
 #-------------------------------------------------------------------------------
 
+def gaussian(x, sigma):
+    return (1/sigma) * ((1/(2*pi))**0.5) * (e**(-0.5 * (x/sigma)**2))
     
 
 #-------------------------------------------------------------------------------
@@ -56,6 +58,13 @@ time_step = 0.0001
 synapse_decay = lookup["synapse_decay"]
 v_th = lookup["v_th"]
 v_r = lookup["v_r"]
+signal_strength = lookup["normal_strength_e"]
+signal_sigma = lookup["normal_sigma_e"]
+dx = lookup["dx"]
+print("beta = {}, v_th = {}, v_r = {}, strength = {}, sigma = {}, dx = {}".format(
+    synapse_decay, v_th, v_r, signal_strength, signal_sigma, dx))
+
+print("I[0]:", I[0])
 
 while simulation_time < 2:
     simulation_time += time_step
@@ -64,14 +73,18 @@ while simulation_time < 2:
         synapse[n] -= time_step * synapse_decay * synapse[n]
     for n in range(neurons_number):
         if voltage[n] >= v_th:
-            voltage[n] = 0
+            voltage[n] = v_r
             spike_count += 1
             spike_id.append(n)
             spike_time.append(simulation_time)
             for m in range(neurons_number):
                 if n != m:
+                    #distance = dx * abs(n - m)
                     distance = Control.d.get_distance_between(coordinates, n, m)
-                    synapse[m] += Control.c.connection_weight(distance)
+                    strength = synapse_decay * dx * signal_strength \
+                               * gaussian(distance, signal_sigma)
+                    synapse[m] += strength
+                    #synapse[m] += Control.c.connection_weight(distance)
 
 
 
@@ -111,7 +124,7 @@ plt.figure(figsize=(8, 8))#, dpi=1000)
 x_axis = numpy.array([coordinates[n, 0] for n in spike_id])
 y_axis = numpy.array(spike_time)
 plt.scatter(x_axis, y_axis, s=0.2, c="#a0a070")
-plt.title("Neuron firing times")
+plt.title("Euler version")
 plt.xlabel("Neuron id/position")
 plt.ylabel("Time")
 plt.margins(x=0, y=0.01)
