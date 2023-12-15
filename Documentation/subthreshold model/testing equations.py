2398a1e90858b1bcf4d1249ee8eab59eb58f5f9d
HKNeuro <hdjk201@exeter.ac.uk>
2023-12-15 13:16:54 +0000

    Update testing equations.py

--- Documentation/subthreshold model/testing equations.py
+++ Documentation/subthreshold model/testing equations.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import e, cos, sin, cosh, sinh
 
-steps = 500
+steps = 5000
 length = 5
 
 delta = length/steps
@@ -10,17 +10,17 @@ t_values = np.arange(0, length, delta)
 v_values = np.zeros(steps)
 u_values = np.zeros(steps)
 
-v_0 = 0
-u_0 = -5
+v_0 = 0.9
+u_0 = 0
 v_values[0] = v_0
 u_values[0] = u_0
 
-s_0 = 5
-I = 5
+s_0 = 1
+I = 1
 beta = 2
 
-C = 20
-D = 0.1
+C = 2
+D = 1
 
 def dv(v, u, t):
     return I - v - u + (s_0 * e**(-beta * t))
@@ -28,6 +28,7 @@ def dv(v, u, t):
 def du(v, u, t):
     return C*v - D*u
 
+#Euler evolution for the subthreshold system
 for x in range(1, steps):
     v = v_values[x-1]
     u = u_values[x-1]
@@ -38,6 +39,7 @@ for x in range(1, steps):
 #------------------------------------------------------------------------------
     
 v_analytic = np.zeros(steps)
+v_analytic_alt = np.zeros(steps)
 w_analytic = np.zeros(steps)
 
 p = 0.5*(D+1)
@@ -75,10 +77,25 @@ def s(x):
         return sinh(x)
 
 def ana_v_pq(t):
+    #See equation 8.17
     s_cluster = s_0 * (2*p - beta - 1) / (p**2 - q2 -2*p*beta + beta**2)
     I_cluster = I * (2*p - 1) / (p**2 - q2)
-    part_c = v_0 - s_cluster - I_cluster
+    part_c = s_cluster + I_cluster - v_0
     part_c *= e**(-p * t) * c(abs_q * t)
+
+    part_s = s_0 * (p**2 + q2 - p*(beta + 1) + beta) / (p**2 - q2 -2*p*beta + beta**2)
+    part_s += I * (p**2 + q2 - p) / (p**2 - q2) + v_0 * (1 - p) + u_0
+    part_s *= e**(-p * t) * s(abs_q * t) / abs_q    #suspect
+
+    part_beta = e**(-beta * t) * s_cluster
+    return part_c - part_s + part_beta + I_cluster
+
+def ana_v_pq_alt(t):
+    #See equation 8.18
+    s_cluster = s_0 * (2*p - beta - 1) / (p**2 - q2 -2*p*beta + beta**2)
+    I_cluster = I * (2*p - 1) / (p**2 - q2)
+    part_c = s_cluster + I_cluster - v_0
+    part_c *= -1 * e**(-p * t) * c(abs_q * t)
 
     part_s = s_0 * (p**2 + q2 - p*(beta + 1) + beta) / (p**2 - q2 -2*p*beta + beta**2)
     part_s += I * (p**2 + q2 - p) / (p**2 - q2) + v_0 * (1 - p) + u_0
@@ -90,6 +107,7 @@ def ana_v_pq(t):
 
 for x in range(0, steps):
     v_analytic[x] = ana_v_pq(t_values[x])
+    v_analytic_alt[x] = ana_v_pq_alt(t_values[x])
 
 #------------------------------------------------------------------------------
 
@@ -97,7 +115,7 @@ plt.figure()
 x_axis = t_values
 y1_axis = v_values
 y2_axis = u_values
-y3_axis = v_analytic
+y3_axis = v_analytic_alt
 plt.plot(x_axis, y1_axis, c="#ff0000")
 plt.plot(x_axis, y2_axis, c="#009900")
 plt.scatter(x_axis, y3_axis, c="#000000")
