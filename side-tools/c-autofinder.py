5e4ff2d462e0a12f64b398e39b06420a0fdf8859
HKNeuro <hdjk201@exeter.ac.uk>
2024-03-07 20:35:17 +0000

    Update c-autofinder.py
    WIP for applying the lessons of numerics from ssLIF integration to the sLIF.  Doesn't seem to be working, though.

--- side-tools/c-autofinder.py
+++ side-tools/c-autofinder.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from math import e, erf, pi
 
 #Shouldn't be 1 for this program, just use 1.0001 or similar
-beta = 2
-A = 0.8
+beta = 1.5
+A = 2
 a = 1
-B = 0.4
+B = 2
 b = 2
 
-is_2D = True
+is_2D = False
 target = 0.1    #v_th - I
 
 #If in 2D:
@@ -19,7 +19,8 @@ if is_2D:
     B = B * b * ((2*pi)**0.5)
 
 
-#The follows is based on formula 8.17 in running review
+#------------------------------------------------------------------------------
+#The follows is based on formula 7.17 in running review
 #May differ from past versions in factors of (2*pi)**0.5 for Z
 
 def sub_part(value):
@@ -32,6 +33,33 @@ def part(Z, z, c):
 
 def whole(c):
     return part(A, a, c) - part(B, b, c)
+#------------------------------------------------------------------------------
+#Alt version to solve the loss of precision
+
+def calc_integral(z, c, param):
+    length = 5 #3*sigma
+    divisions = 5000
+    dT = length/divisions
+    total = 0
+    t = 0
+    for x in range(divisions):
+        T = t - (x+0.5)*dT
+        value = e**(-(0.5 * (c/z)**2 * T**2) + (T - t))
+        total += dT * value
+    return total
+
+def part_alt(Z, z, c):
+    coeff = Z * (beta / (1 - beta)) * (c / (z * (2*pi)**0.5))
+    return coeff * (calc_integral(z, c, beta) - calc_integral(z, c, 1))
+    
+
+def whole_alt(c):
+    return part_alt(A, a, c) - part(B, b, c)
+
+#Not lining up?  Doesn't seem to have solved the numerics anyway
+
+#------------------------------------------------------------------------------
+
 
 #Assume the function is decreasing
 #This is probably going to be a bit of a hack for now
@@ -58,11 +86,14 @@ points = 200
 x_values = numpy.linspace(c - 0.2, c + 0.2, points)
 x_values = numpy.linspace(0.2, 5, points)
 y_values = numpy.zeros(points)
+y_alt_values = numpy.zeros(points)
 for key in range(points):
     y_values[key] = whole(x_values[key])
+    y_alt_values[key] = whole_alt(x_values[key])
 
 plt.figure()
 plt.plot(x_values, y_values)
+plt.plot(x_values, y_alt_values)
 plt.axhline(y = target)
 
 plt.title("Speed finder, $(A, a, B, b)$ = ({}, {}, {}, {}), $\\beta$ = {}".format(
