5ebff86bfac470fc81b1e0acace20af946dc32bb
HKNeuro <hdjk201@exeter.ac.uk>
2024-03-13 20:04:36 +0000

    Bugfixing tests for the multi-spike wave

---
+++ Documentation/subthreshold model/multi-spike wave - ec2.py
@@ -0,0 +1,287 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import e, erf, pi, cos, sin
+
+beta = 4
+A = 2
+a = 1
+B = 2
+b = 3
+
+I = 3.4
+
+C = 6
+D = 2
+
+c = 3.24
+firing_times = [0]
+u_at_firing_times = firing_times[:]
+
+
+dx = 0.001
+neurons_number = 2000
+
+v_r = 0
+
+#Derived values
+p = 0.5*(D+1)
+q = 0.5*( (D-1)**2 -4*C )**0.5
+if type(q) != type((-1)**0.5):
+    print("q is", q)
+    print("q should be imaginary for this to work")
+    raise TypeError
+abs_q = abs(q)
+q2 = -abs_q**2
+
+#----------------------------------------------------------------------------
+
+def v_after(t):
+    values_I =   I * ((2*p - 1) / (p**2 - q2))        \
+               * (1 - e**(-p * t) * cos(abs_q * t))   \
+               - I * (p**2 + q2 - p)                  \
+               / ((p**2 - q2) * abs_q) * e**(-p*t) * sin(abs_q * t)
+
+    values_init = e**(-p * t)                   \
+                  * ( v_r * cos(abs_q * t)      
+                     - ((v_r * (1 - p) + u(0)) / abs_q) * sin(abs_q * t))
+
+    return values_I + values_init + beta * (part_v_after(A, a, t) -
+                                            part_v_after(B, b, t))
+
+def part_v_after(Z, z, t):
+    coeff_diff = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sum = (p**2 + q2 - p*(beta + 1) + beta) \
+                / (((p - beta)**2 - q2) * abs_q)
+
+    cos_inside = coeff_diff * calc_integral_old(z, t, p, "cosine", lower = 0)
+    sin_inside = coeff_sum  * calc_integral_old(z, t, p, "sine", lower = 0)
+    part_beta  = coeff_diff * calc_integral_old(z, t, beta, None)
+    trig_outside = (coeff_diff * cos(abs_q * t)
+                    + coeff_sum * sin(abs_q * t)) \
+                    * calc_integral_old(z, t, beta, None, upper = 0, param2 = p)
+    return Z * (-cos_inside + sin_inside + part_beta - trig_outside)
+                    
+
+def calc_integral_old(z, t, param, func_id,
+                      lower = None, upper = None, param2 = None):
+    if upper == None:
+        upper = t
+    if param2 == None:
+        param2 = param
+        
+    if func_id == "sine":
+        func = sin
+    elif func_id == "cosine":
+        func = cos
+    else:
+        func = one
+
+    #sigma = z/c
+    #mu = sigma**2 * param
+
+    divisions = 500
+    max_length = 5 #3*sigma
+    if lower != None and upper - lower < max_length:
+        length = upper - lower
+    else:
+        length = max_length
+    dT = length/divisions
+    total = 0
+    for x in range(divisions):
+        T = upper - (x+0.5)*dT  #Midpoint method
+        value = func(abs_q * (T - t))     \
+                * (c / (z * (2*pi)**0.5)) \
+                * e**( -(c**2 / (2*z**2)) * T**2 + param*T - param2*t)
+        total += dT * value
+    return total
+
+#-----------------------------------------------------------------------------
+#General integration function
+
+def one(x):
+    return 1
+
+def calc_integral(t, mu, sigma, param, param2 = None,
+                  func_name = None, lower = None, upper = None):
+    #Parse the optional inputs
+    if func_name == "sine":
+        func = sin
+    elif func_name == "cosine":
+        func = cos
+    else:
+        func = one
+    if param2 == None:
+        param2 = param
+    if upper == None:
+        upper = t
+        
+    #Window of integration
+    if lower == None:
+        #lower = upper - 3*sigma
+        lower = upper - 5
+    else:
+        #lower = max(upper - 3*sigma, lower)
+        lower = max(upper - 5, lower)
+    divisions = 100
+    dT = (upper - lower) / divisions
+    total = 0
+    for x in range(divisions):
+        T = lower + dT * (x + 0.5)
+        total += func(abs_q * (t - T))  \
+                 * e**(-0.5 * ((T - mu)/sigma)**2 - param*t + param2*T)
+    total *= dT / (sigma * (2*pi)**0.5)
+    return total
+
+#-----------------------------------------------------------------------------
+#Calculations for s
+
+def s(t):
+    return beta * (part_s(A, a, t) - part_s(B, b, t))
+
+def part_s(Z, z, t):
+    total = 0
+    sigma = z / c
+    for mu in firing_times:
+        total += calc_integral(t, mu, sigma, beta)
+    total *= Z
+    return total
+
+#-----------------------------------------------------------------------------
+#Calculations for v
+
+def v(t, t_old = None, u_old = None):
+    coeff_cos = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sin = (p**2 + q2 - p*(beta + 1) + beta) / (((p - beta)**2 - q2)*abs_q)
+    
+    part_I = I * (2*p - 1) / (p**2 - q2)
+    part_init = 0
+    if t_old != None:
+        t_old = 0
+        part_I *= 1 - e**(-p * (t - t_old)) * cos(abs_q * (t - t_old))
+        part_I -= I * (p**2 + q2 - p) / ((p**2 - q2) * abs_q)           \
+                  * e**(-p * (t - t_old)) * sin(abs_q * (t - t_old))
+        part_init = e**(-p * (t - t_old))       \
+                    * (v_r * cos(abs_q * (t - t_old))
+                       - ((v_r*(1 - p) + u_old) / abs_q)
+                          * sin(abs_q * (t - t_old)))
+    part_s = s(t) * coeff_cos
+
+    coeff_cos = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sin = (p**2 + q2 - p*(beta + 1) + beta) / (((p - beta)**2 - q2)*abs_q)
+    part_t_old = 0
+    if t_old != None:
+        part_t_old = s(t_old) * e**(-p * (t - t_old))       \
+                     * (  coeff_cos * cos(abs_q * (t - t_old))
+                        + coeff_sin * sin(abs_q * (t - t_old)))
+
+    return part_I + part_init + part_s - part_t_old \
+           + part_v(A, a, t, t_old) - part_v(B, b, t, t_old)
+
+def part_v(Z, z, t, t_old):
+    coeff_cos = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sin = (p**2 + q2 - p*(beta + 1) + beta) / (((p - beta)**2 - q2)*abs_q)
+
+    sigma = z / c
+    part_cos = 0
+    part_sin = 0
+    for mu in firing_times:
+        part_cos += calc_integral(t, mu, sigma, p,
+                                  func_name = "cosine", lower = t_old)
+        part_sin += calc_integral(t, mu, sigma, p,
+                                  func_name = "sine", lower = t_old)
+    part_cos *= coeff_cos
+    part_sin *= coeff_sin
+    return -beta * Z * (part_cos + part_sin)
+
+#-----------------------------------------------------------------------------
+#Calculations for u
+
+def u(t, t_old = None, u_old = None):
+    coeff_cos = 1 / ((p - beta)**2 - q2)
+    coeff_sin = (p - beta) / (((p - beta)**2 - q2) * abs_q)
+    
+    part_I = C * I / (p**2 - q2)
+    part_init = 0
+    if t_old != None:
+        part_I *= 1 - e**(-p * (t - t_old)) * cos(abs_q * (t - t_old))
+        part_I -= C * I * (p / ((p**2 - q2) * abs_q))   \
+                  * e**(-p * (t - t_old)) * sin(abs_q * (t - t_old))
+        part_init = e**(-p * (t - t_old))       \
+                    * (u_old * cos(abs_q * (t - t_old))
+                       + ((C * v_r + u_old * (1 - p)) / abs_q)
+                          * sin(abs_q * t - t_old))
+    part_s = s(t) * C * coeff_cos
+
+    part_t_old = 0
+    if t_old != None:
+        part_t_old = s(t_old) * e**(-p * (t - t_old))       \
+                     * C * (  coeff_cos * cos(abs_q * (t - t_old))
+                            + coeff_sin * sin(abs_q * (t - t_old)))
+        
+    return part_I + part_init + part_s - part_t_old \
+           + part_u(A, a, t, t_old) - part_u(B, b, t, t_old)
+
+def part_u(Z, z, t, t_old):
+    coeff_cos = 1 / ((p - beta)**2 - q2)
+    coeff_sin = (p - beta) / (((p - beta)**2 - q2) * abs_q)
+
+    sigma = z / c
+    part_cos = 0
+    part_sin = 0
+    for mu in firing_times:
+        part_cos += calc_integral(t, mu, sigma, p,
+                                   func_name = "cosine", lower = t_old)
+        part_sin += calc_integral(t, mu, sigma, p,
+                                   func_name = "sine", lower = t_old)
+    part_cos *= coeff_cos
+    part_sin *= coeff_sin
+    return -C * beta * Z * (part_cos + part_sin)
+    
+#-----------------------------------------------------------------------------
+
+steps = 800
+t_values = np.linspace(-4, 4, steps)
+voltage = np.zeros(steps)
+wigglage = np.zeros(steps)
+synapse = np.zeros(steps)
+
+#Finding values of u at firing times
+for k, t in enumerate(firing_times):
+    if k == 0:
+        u_at_firing_times[k] = u(t)
+    else:
+        u_at_firing_times[k] = u(t, firing_times[k-1], u_at_firing_times[k-1])
+
+for n in range(steps):
+    t = t_values[n]
+    t_last = None
+    u_last = None
+    for k, firing_time in enumerate(firing_times):
+        if firing_time < t:
+            t_last = firing_time
+            u_last = u_at_firing_times[k]
+    voltage[n] = v(t, t_last, u_last)
+    #voltage[n] = v(t)
+    wigglage[n] = u(t, t_last, u_last)
+    #wigglage[n] = u(t)
+    synapse[n] = s(t)
+    if t > 0:
+        synapse[n] = v_after(t)
+
+plt.figure()
+plt.plot(t_values, voltage, c="#0033dd", label="$v$")
+plt.plot(t_values, synapse, c="#33dd00", label="$s$")
+plt.plot(t_values, wigglage, c="#dd0033", label="$u$")
+
+plt.title("""\n
+Variables for  ssLIF multi-spike wave, $c$ = {}
+""".format(c))
+plt.xlabel("Time")
+plt.ylabel("Value")
+
+plt.axhline(1, linestyle="dashed", c="#999999", label="$v_{th}$")
+plt.axhline(0, linestyle="dotted", c="#999999", label="$v_r$")
+plt.axvline(0, c="#999999")
+plt.legend(loc = "upper left")
+plt.show()
+
