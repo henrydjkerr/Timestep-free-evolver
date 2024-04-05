2d372012c24684559bc7069c41dddc03fd9d8f03
HKNeuro <hdjk201@exeter.ac.uk>
2024-04-05 17:49:25 +0100

    Create mass root finder.py
    Tool for plotting the viable wave speeds for a wave as C varies.  Currently only supports 2-spike waves for convenience.

---
+++ Documentation/subthreshold model/mass root finder.py
@@ -0,0 +1,270 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from math import e, erf, pi, cos, sin, log
+from scipy import optimize
+
+beta = 6
+A = 2
+a = 1
+B = 2
+b = 2
+
+D = 1
+
+v_th = 1
+v_r = 0
+
+C_min = 0.001
+C_max = 3.001
+C_steps = 30
+x_count = 6
+y_count = 12
+
+def update_C(k):
+    global C
+    global I
+    global p
+    global q2
+    global abs_q
+
+    C = C_min + (C_max - C_min) * (k/C_steps)
+    I = 0.90 * (C + D) / D
+    #Derived values
+    p = 0.5*(D+1)
+    q = 0.5*( (D-1)**2 -4*C )**0.5
+    if type(q) != type((-1)**0.5):
+        print("q is", q)
+        print("q should be imaginary for this to work")
+        raise TypeError
+    abs_q = abs(q)
+    q2 = -abs_q**2
+    return
+
+update_C(0)
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#General integration function
+
+def one(x):
+    return 1
+
+def calc_integral(t, mu, sigma, param, param2 = None,
+                  func_name = None, lower = None, upper = None):
+    #Parse the optional inputs
+    if func_name == "sine":
+        func = sin
+    elif func_name == "cosine":
+        func = cos
+    else:
+        func = one
+    if param2 == None:
+        param2 = param
+    if upper == None:
+        upper = t
+        
+    #Window of integration
+    if lower == None:
+        #lower = upper - 3*sigma
+        lower = upper - 5
+    else:
+        #lower = max(upper - 3*sigma, lower)
+        lower = max(upper - 5, lower)
+    divisions = 100
+    dT = (upper - lower) / divisions
+    total = 0
+    for x in range(divisions):
+        T = lower + dT * (x + 0.5)
+        total += func(abs_q * (t - T))  \
+                 * e**(-0.5 * ((T - mu)/sigma)**2 - param*t + param2*T)
+    total *= dT / (sigma * (2*pi)**0.5)
+    return total
+
+#-----------------------------------------------------------------------------
+#Calculations for s
+
+def s(t):
+    return beta * (part_s(A, a, t) - part_s(B, b, t))
+
+def part_s(Z, z, t):
+    total = 0
+    sigma = z / c
+    for mu in firing_times:
+        total += calc_integral(t, mu, sigma, beta)
+    total *= Z
+    return total
+
+#-----------------------------------------------------------------------------
+#Calculations for v
+
+def v(t, t_old = None, u_old = None):
+    coeff_cos = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sin = (p**2 + q2 - p*(beta + 1) + beta) / (((p - beta)**2 - q2)*abs_q)
+    
+    part_I = I * (2*p - 1) / (p**2 - q2)
+    part_init = 0
+    if t_old != None:
+        part_I *= 1 - e**(-p * (t - t_old)) * cos(abs_q * (t - t_old))
+        part_I -= I * (p**2 + q2 - p) / ((p**2 - q2) * abs_q)           \
+                  * e**(-p * (t - t_old)) * sin(abs_q * (t - t_old))
+        part_init = e**(-p * (t - t_old))       \
+                    * (v_r * cos(abs_q * (t - t_old))
+                       - ((v_r*(1 - p) + u_old) / abs_q)
+                          * sin(abs_q * (t - t_old)))
+    part_s = s(t) * coeff_cos
+
+    coeff_cos = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sin = (p**2 + q2 - p*(beta + 1) + beta) / (((p - beta)**2 - q2)*abs_q)
+    part_t_old = 0
+    if t_old != None:
+        part_t_old = s(t_old) * e**(-p * (t - t_old))       \
+                     * (  coeff_cos * cos(abs_q * (t - t_old))
+                        + coeff_sin * sin(abs_q * (t - t_old)))
+
+    return part_I + part_init + part_s - part_t_old \
+           + part_v(A, a, t, t_old) - part_v(B, b, t, t_old)
+
+def part_v(Z, z, t, t_old):
+    coeff_cos = (2*p - beta - 1) / ((p - beta)**2 - q2)
+    coeff_sin = (p**2 + q2 - p*(beta + 1) + beta) / (((p - beta)**2 - q2)*abs_q)
+
+    sigma = z / c
+    part_cos = 0
+    part_sin = 0
+    for mu in firing_times:
+        part_cos += calc_integral(t, mu, sigma, p,
+                                  func_name = "cosine", lower = t_old)
+        part_sin += calc_integral(t, mu, sigma, p,
+                                  func_name = "sine", lower = t_old)
+    part_cos *= coeff_cos
+    part_sin *= coeff_sin
+    return -beta * Z * (part_cos + part_sin)
+
+#-----------------------------------------------------------------------------
+#Calculations for u
+
+def u(t, t_old = None, u_old = None):
+    coeff_cos = 1 / ((p - beta)**2 - q2)
+    coeff_sin = (p - beta) / (((p - beta)**2 - q2) * abs_q)
+    
+    part_I = C * I / (p**2 - q2)
+    part_init = 0
+    if t_old != None:
+        part_I *= 1 - e**(-p * (t - t_old)) * cos(abs_q * (t - t_old))
+        part_I -= C * I * (p / ((p**2 - q2) * abs_q))   \
+                  * e**(-p * (t - t_old)) * sin(abs_q * (t - t_old))
+        part_init = e**(-p * (t - t_old))       \
+                    * (u_old * cos(abs_q * (t - t_old))
+                       + ((C * v_r + u_old * (1 - p)) / abs_q)
+                          * sin(abs_q * (t - t_old)))
+    part_s = s(t) * C * coeff_cos
+
+    part_t_old = 0
+    if t_old != None:
+        part_t_old = s(t_old) * e**(-p * (t - t_old))       \
+                     * C * (  coeff_cos * cos(abs_q * (t - t_old))
+                            + coeff_sin * sin(abs_q * (t - t_old)))
+##
+##    print("u_old", u_old)
+##    print("part_I", part_I)
+##    print("part_init", part_init)
+##    print("part_s", part_s)
+##    print("part_t_old", part_t_old)
+##    print("part_A", part_u(A, a, t, t_old))
+##    print("part_B", part_u(B, b, t, t_old))
+##        
+    return part_I + part_init + part_s - part_t_old \
+           + part_u(A, a, t, t_old) - part_u(B, b, t, t_old)
+
+def part_u(Z, z, t, t_old):
+    coeff_cos = 1 / ((p - beta)**2 - q2)
+    coeff_sin = (p - beta) / (((p - beta)**2 - q2) * abs_q)
+
+    sigma = z / c
+    part_cos = 0
+    part_sin = 0
+    for mu in firing_times:
+        part_cos += calc_integral(t, mu, sigma, p,
+                                   func_name = "cosine", lower = t_old)
+        part_sin += calc_integral(t, mu, sigma, p,
+                                   func_name = "sine", lower = t_old)
+    part_cos *= coeff_cos
+    part_sin *= coeff_sin
+    return -C * beta * Z * (part_cos + part_sin)
+    
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+spikes = 2
+
+#def root_function(c_try, t_1_try):
+def root_function(inputs):
+    global c
+    global firing_times
+    c = inputs[0]
+    t = inputs[1]
+    firing_times = [0, t]
+    error_1 = v(0) - v_th
+    u_old = u(0)
+    error_2 = v(t, 0, u_old) - v_th
+    return [error_1, error_2]
+
+
+#------------------------------------------------------------------------------
+
+
+results_x = []
+results_y = []
+for n in range(C_steps):
+    print(n)
+    update_C(n)
+    for init_c in np.linspace(0.1, 3.1, x_count):
+        for init_t in np.linspace(0.3, 8.5, y_count):
+            guess = [init_c, init_t]
+            soln = optimize.root(root_function, guess, tol=0.00002)
+            if soln.success and soln.x[0] > 0 and soln.x[1] > 0:
+                results_x.append(C)
+                results_y.append(soln.x[0])
+
+##counter = 0
+##for init_c in np.linspace(0.1, 3.1, x_count):
+##    for init_t in np.linspace(0.5, 8.5, y_count):
+##        print(counter)
+##        counter += 1
+##        guess = [init_c, init_t]
+##        for n in range(C_steps):
+##            update_C(n)
+##            soln = optimize.root(root_function, guess)
+##            if soln.success and soln.x[0] > 0 and soln.x[1] > 0:
+##                results_x.append(C)
+##                results_y.append(soln.x[0])
+##                guess = soln.x
+
+plt.figure()
+plt.scatter(results_x, results_y, s=0.4)
+plt.xlabel("$C$")
+plt.ylabel("Wave speed $c$")
+plt.title("Wave solutions by speed for given $C$")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
