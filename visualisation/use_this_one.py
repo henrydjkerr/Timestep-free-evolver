edc0a3d308c1b9c87a308b3abf393dc62c731dde
HKNeuro <hdjk201@exeter.ac.uk>
2025-11-04 17:35:07 +0000

    Renaming thing.py
    Changing to a slightly more descriptive name

--- visualisation/use_this_one.py
+++ visualisation/use_this_one.py
@@ -1,10 +1,12 @@
 #from display_1D_print import make_graph
 from display_3D_xyt import make_graph
 #from display_video_frames import make_graph
+#from display_colourmap import make_graph
 
 import time
 stopwatch = time.time()
 
-make_graph("output-20250612145739-10000N-20000sp")
+#make_graph("output-20250612145739-10000N-20000sp")
+make_graph("output-20250612144739-10000N-10000sp")
 
 print(time.time() - stopwatch)
